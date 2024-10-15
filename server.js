ac42ec1f628436e52f4bb2354400b3725feb61f8
Brandon Hamilton <bham2015@gmail.com>
2024-10-15 12:16:13 -0500

    Completed Guided Practice.

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const prisma = require("./prisma");
 
 app.use(require("morgan")("dev"));
 app.use(express.json());
 
 // TODO: POST /reservations
+app.post("/reservations", async (req, res, next) => {
+  try {
+    const { date, restaurantId, customerIds } = req.body;
+    const party = customerIds.map((id) => ({ id: +id }));
+    console.log(party);
+
+    const reservation = await prisma.reservation.create({
+      data: {
+        date,
+        restaurantId: +restaurantId,
+        party: { connect: party },
+      },
+      include: {
+        restaurant: true,
+        party: true,
+      },
+    });
+    res.status(201).json(reservation);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
